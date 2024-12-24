d5afb8ac7cd0d4602369386ac29de33fe0edd37b
StuartWalker-creator <stuartwalker041@gmail.com>
2024-12-24 12:53:19 +0300

    Update amazon.js

--- amazon.js
+++ amazon.js
@@ -21,7 +21,7 @@ function renderProducts(products){
 
           <div class="product-rating-container">
           <img class="product-rating-stars"
-          src="images/ratings/rating-${product.rating.stars*10}.png">
+          src="rating-${product.rating.stars*10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
